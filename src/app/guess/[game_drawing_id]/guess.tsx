edbe29930ebe3c7471e8145ffff0cf5b8de23ff7
Eugene Fairley <ecfairley@ucdavis.edu>
2025-01-02 14:54:03 -0800

    Better Guess page UI

--- src/app/guess/[game_drawing_id]/guess.tsx
+++ src/app/guess/[game_drawing_id]/guess.tsx
@@ -1,6 +1,8 @@
 "use client";
 import {GameDrawing} from "@/lib/data_definitions";
 import {useRouter} from "next/navigation";
+import { Button } from '@/components/button'
+import { SendHorizonal } from 'lucide-react'
 
 
 export default function Guess({gameDrawing, imageUrl} : {gameDrawing: GameDrawing, imageUrl: string}) {
@@ -25,12 +27,17 @@ export default function Guess({gameDrawing, imageUrl} : {gameDrawing: GameDrawin
     }
 
     return (
-            <div>
-                <form method="post" onSubmit={handleSubmit}>
+            <div className='container mx-auto max-w-fit'>
+                <form method="post" onSubmit={handleSubmit} className='text-3xl justify-center text-center'>
                     <label>Guess: <input name="guess" autoComplete="off"/></label>
-                    <button type="submit">Submit</button>
+                    <Button
+                        size='lg'
+                        type='submit'
+                        color={''}>
+                        <SendHorizonal/>
+                    </Button>
                 </form>
-                <img alt='drawing' src={`${imageUrl}`}/>
+                <img alt='drawing' src={`${imageUrl}`} className='border border-black'/>
             </div>
         )
 }
