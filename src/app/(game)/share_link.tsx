00269f9022421c1ba1116988d6d9c7d5764be288
Eugene Fairley <ecfairley@ucdavis.edu>
2025-01-24 15:43:36 -0800

    Configured site url for share links

--- src/app/(game)/share_link.tsx
+++ src/app/(game)/share_link.tsx
@@ -4,6 +4,11 @@ import ButtonOverlay from "@/components/ButtonOverlay";
 
 export default function ShareLink({roomId}: {roomId: string}) {
     return (
-        <ButtonOverlay overlayText={'Copied Room Link'} className='w-24 h-24 mt-10' onClick={() => {navigator.clipboard.writeText(`localhost:3000/join_room/${roomId}`)}}><Plus></Plus></ButtonOverlay>
+        <ButtonOverlay overlayText={'Copied Room Link'} className='w-24 h-24 mt-10'
+                       onClick={() => {
+                           navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_SITE_URL}/join_room/${roomId}`)
+                       }}>
+            <Plus/>
+        </ButtonOverlay>
     )
 }
