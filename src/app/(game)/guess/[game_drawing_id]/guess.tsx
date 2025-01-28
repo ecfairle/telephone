b40b337b8db70442a262a5eba530b4dbe02593a7
Eugene Fairley <ecfairley@ucdavis.edu>
2025-01-28 10:03:21 -0800

    Await guess_save API call before redirect

--- src/app/(game)/guess/[game_drawing_id]/guess.tsx
+++ src/app/(game)/guess/[game_drawing_id]/guess.tsx
@@ -7,12 +7,12 @@ import { SendHorizonal } from 'lucide-react'
 
 export default function Guess({gameDrawing, imageUrl} : {gameDrawing: GameDrawing, imageUrl: string}) {
     const router = useRouter();
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
         const form = e.target;
         const formData = new FormData(form as HTMLFormElement);
-        fetch(`/save_guess/${gameDrawing.id}`, {
+        await fetch(`/save_guess/${gameDrawing.id}`, {
             method: 'POST',
                 headers: {
                 'Accept': 'application/json', 'Content-Type': 'application/json'
