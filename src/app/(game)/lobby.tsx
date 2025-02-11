dacc5949fbc736d00e024fc5c6e1de8a9de5adea
Eugene Fairley <ecfairley@ucdavis.edu>
2025-02-11 12:00:44 -0800

    Add how to play info in lobby

--- src/app/(game)/lobby.tsx
+++ src/app/(game)/lobby.tsx
@@ -15,6 +15,7 @@ const colors = [
 export default function Lobby({roomId, userId, users, gamesMap} :{roomId: string, userId: string, users: User[], gamesMap:{ [game_id: string]: {name: string}[] }}) {
     const [roomies, setRoomies] = useState<User[]>(users);
     const [games, setGames] = useState(gamesMap);
+    const [startGameEnabled, setStartGameEnabled] = useState(Object.keys(games).length === 0 && roomies.length > 1);
 
     useEffect(() => {
         const fetchRoomData = async () => {
@@ -23,6 +24,7 @@ export default function Lobby({roomId, userId, users, gamesMap} :{roomId: string
                 const result : {games: { [game_id: string]: {name: string}[] }, roomies: User[], room_id: string} = await res.json();
                 setGames(result['games']);
                 setRoomies(result['roomies']);
+                setStartGameEnabled(Object.keys(games).length === 0 && roomies.length > 1);
             } catch(error) {
                 console.log('error' + error)
             }
@@ -47,25 +49,39 @@ export default function Lobby({roomId, userId, users, gamesMap} :{roomId: string
     const session = useSession();
 
     const userColors = roomies.reduce((prev, cur, idx) => ({[cur.name]: colors[idx],  ...prev}), {});
-    return (<div className={"ml-5  container"}>
-            <div className={'flex flex-row'}>
+    return (<div className={"flex flex-col container p-5"}>
+            <div className={''}>
                 {(Object.keys(games).length > 0 || roomies.length > 1) && <Button className={'mr-5'} onClick={handleLeaveRoomClick}>Leave Room</Button>}
-                {Object.keys(games).length === 0 && roomies.length > 1 && <Button onClick={handleNewGameClick}>Start Game</Button>}
+                {Object.keys(games).length === 0 && <Button disabled={!startGameEnabled} onClick={handleNewGameClick}>Start Game</Button>}
             </div>
 
             {Object.keys(games).length === 0 ?
-                <div className={"flex-row"}>
-                    <div className={"flex flex-col"}>
+                <div>
+                    <div className={""}>
                         {roomies.map((user) => {
-                            return(
-                                <div key={user.id} className='w-24 h-24 mt-10 truncate'><UserTag userColors={userColors} name={user.name}/>
-                                    <img className={'w-12 h-12'} src={user.image} />
-                                </div>)})
+                            return (
+                                <div key={user.id} className='w-24 h-24 mt-10 truncate'><UserTag userColors={userColors}
+                                                                                                 name={user.name}/>
+                                    <img className={'w-12 h-12'} src={user.image}/>
+                                </div>)
+                        })
                         }
-                        {Array.from({length: 5-roomies.length}, (v,k)=>k+1).map((idx) => (
+                        {Array.from({length: 5 - roomies.length}, (v, k) => k + 1).map((idx) => (
                             <div key={idx}><ShareLink roomId={roomId}></ShareLink>
                             </div>
                         ))}
+                    </div>
+                    <div className={'border border-black text-center justify-center mt-5 p-2'}>
+                        <h2>How to play:</h2>
+                        <br/>
+                        Join a room with 1-4 friends to play. Every day there are a new set of words. When you start the
+                        game,
+                        a word will be randomly assigned to each player. <br/>For each word, the first player will draw
+                        that word. Then, the next
+                        player in order will guess the
+                        first player's drawing, and the next player will draw the second player's guess. And so on. At
+                        the end,
+                        see how the original word morphed being passed between everyone!
                     </div>
                 </div>
                 :
@@ -81,6 +97,6 @@ export default function Lobby({roomId, userId, users, gamesMap} :{roomId: string
     )
 }
 
-function UserTag({userColors, name}:{userColors:{[name:string]: string }, name:string}) {
-    return (<span className={userColors[name]}><strong>{name}</strong></span> )
+function UserTag({userColors, name}: { userColors: { [name: string]: string }, name: string }) {
+    return (<span className={userColors[name]}><strong>{name}</strong></span>)
 }
