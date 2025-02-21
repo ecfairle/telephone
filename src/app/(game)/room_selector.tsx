e5d0f612576384059446d4e25a3ebf140f8a7284
Eugene Fairley <ecfairley@ucdavis.edu>
2025-02-20 23:48:38 -0800

    Prevent spam clicks on new room

--- src/app/(game)/room_selector.tsx
+++ src/app/(game)/room_selector.tsx
@@ -3,9 +3,26 @@ import {Button} from "@/components/button";
 import {useRouter} from "next/navigation";
 import {useSession} from "next-auth/react";
 import {createRoom} from "@/lib/api";
+import {useState} from "react";
+
+const MAX_ROOMS = 5;
 
 export default function RoomSelector({userRooms, roomId} : {userRooms: { room_id: string }[], roomId: string|null}) {
+    async function handleNewRoomClick() {
+        try {
+            setNewRoomEnabled(false);
+            const res = await createRoom(session?.user.userId as string);
+            const room_id = await res.json();
+            if (res.ok) {
+                router.push(`/room/${room_id}`);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const router = useRouter();
+    const [newRoomEnabled, setNewRoomEnabled] = useState(userRooms.length < MAX_ROOMS);
     const { data: session } = useSession();
     if (!session?.user.userId) {
         router.push("/login");
@@ -24,11 +41,9 @@ export default function RoomSelector({userRooms, roomId} : {userRooms: { room_id
             </Button>
         ))}
             </div>
-            <Button onClick={async () => {
-                const res = await createRoom(session.user.userId as string);
-                const room_id = await res.json();
-                router.push(`/room/${room_id}`)
-            }}>New Room</Button>
+            <Button
+                disabled={!newRoomEnabled}
+                onClick={handleNewRoomClick}>New Room</Button>
         </div>
     )
 }
--- src/app/create_room/route.ts
+++ src/app/create_room/route.ts
@@ -1,11 +1,17 @@
 import {NextRequest} from "next/server";
-import {createRoom} from "@/lib/data";
+import {createRoom, getRooms} from "@/lib/data";
+
+const MAX_ROOMS = 5;
 
 export async function POST(
     request: NextRequest
 ) {
     const loadedParams = await request.json();
     const userId = loadedParams.user_id;
+    const userRooms = await getRooms(userId);
+    if (userRooms.length >= MAX_ROOMS) {
+        return Response.json("Too many rooms", {status: 400})
+    }
     const room = await createRoom(userId);
     if (room === null) {
         return Response.json("Failed to create room", {status: 400})
