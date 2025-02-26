f5188684e2fdc4479b419bd60712a22261890db5
Eugene Fairley <ecfairley@ucdavis.edu>
2025-02-26 11:03:20 -0800

    Allow join room from link copied from url box

--- src/app/(game)/game_blurb.tsx
+++ src/app/(game)/game_blurb.tsx
@@ -5,7 +5,7 @@ export default function GameBlurb() {
         <div className={'border border-black text-center justify-center mt-5 p-2'}>
             <h2>How to play:</h2>
             <br/>
-            Join a room with 1-4 friends to play. Every day there are a new set of words. When you start the
+            Join a room with 1-4 friends to play. Every day there is a new set of words. When you start the
             game,
             a word will be randomly assigned to each player. <br/>For each word, the first player will draw
             that word. Then, the next
--- src/app/(game)/room/[room_id]/page.tsx
+++ src/app/(game)/room/[room_id]/page.tsx
@@ -4,6 +4,9 @@ import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/auth";
 import Lobby from "@/app/(game)/lobby";
 import RoomSelector from "@/app/(game)/room_selector";
+import {User} from "@/lib/data_definitions";
+import JoinRoom from "@/app/(game)/join_room/[room_id]/join";
+import React from "react";
 
 export default async function Home({
                                        params,
@@ -17,10 +20,17 @@ export default async function Home({
     const roomUsers = await getRoomies(roomId);
     const userRooms = await getRooms(userId);
     const games = await fetchGames(userId, roomId);
+    const userInRoom = roomUsers.find((user: User) => user.id == userId) !== undefined;
     return (
         <div>
+            {userInRoom?
+                <div>
             <RoomSelector userRooms={userRooms} roomId={roomId} />
             <Lobby roomId={roomId} users={roomUsers} gamesMap={games} userId={userId} />
+                </div>
+                :
+                <JoinRoom userId={userId} roomId={roomId}/>
+            }
         </div>
     )
 }
