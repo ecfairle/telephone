cad5beb10867b8dcbf921d53c5baefbb487f9029
Eugene Fairley <ecfairley@ucdavis.edu>
2025-04-03 10:48:05 -0700

    Add updates page for games

--- src/app/(game)/shuffle/shuffle.tsx
+++ src/app/(game)/shuffle/shuffle.tsx
@@ -6,9 +6,11 @@ import Canvas from "@/app/(game)/canvas/[game_drawing_id]/canvas";
 import Guess from "@/app/(game)/guess/[game_drawing_id]/guess";
 import {getShuffleGames} from "@/lib/api";
 import {GameDrawing} from "@/lib/data_definitions";
+import {Button} from "@/components/button";
 
 export default function Shuffle({userId}:{userId:string}) {
     const [games, setGames] = useState<(GameDrawing&{turnUser:null|string, drawTurn:null|boolean, signedUrl: string})[]>([]);
+    const [updatesMode, setUpdatesMode] = useState<boolean>(false);
     useEffect(() => {
         const fetchGamesData = async () => {
             try {
@@ -32,15 +34,21 @@ export default function Shuffle({userId}:{userId:string}) {
             secretWord={drawing.target_word}/>
         else return <Guess gameDrawing={drawing} imageUrl={drawing.signedUrl}/>
     }
+    const updates = pastGames.toSorted(
+        (a,b) => new Date(b.updated_at).valueOf() - new Date(a.updated_at).valueOf())
     return (
         <div>
-            <Pull/>
+            <div className={"space-x-3"}>
+                <Pull/>
+                <Button onClick={()=>setUpdatesMode(false)} disabled={!updatesMode}>All Games</Button>
+                <Button onClick={()=>setUpdatesMode(true)} disabled={updatesMode}>Updates</Button>
+            </div>
             {myTurnGames.map((game, idx) => (
                 <div key={idx}>
                     <GamePanels userColors={{}} userId={userId} gameId={game.game_id}/>
                 </div>
             ))}
-            {pastGames.map((game, idx) => (
+            {(updatesMode? updates : pastGames).map((game, idx) => (
                 <div key={idx}>
                     <GamePanels userColors={{}} userId={userId} gameId={game.game_id}/>
                 </div>
