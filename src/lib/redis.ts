d0976b42add009ce5fb65cc318cea821aa654052
Eugene Fairley <ecfairley@ucdavis.edu>
2025-04-07 16:56:44 -0700

    Update redis connection

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import {createClient} from "redis";
 
-const redis = await createClient({url: process.env.REDIS_URL!})
+const redis = await createClient({url: process.env.REDIS2_REDIS_URL!})
     .on('error', err => console.log('Redis Client Error', err))
     .connect();
 
