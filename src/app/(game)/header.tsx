647871f738bc5a236895a5f50b4849abdb4382a1
Eugene Fairley <ecfairley@ucdavis.edu>
2025-09-12 15:54:20 -0700

    Update site header

--- src/app/(game)/header.tsx
+++ src/app/(game)/header.tsx
@@ -1,23 +1,23 @@
 "use client"
 import {Button} from "@/components/button";
 import {signIn, signOut, useSession} from "next-auth/react";
-import {useRouter} from "next/navigation";
+import {useRouter, usePathname} from "next/navigation";
 
 export default function Header() {
     const { data: session } = useSession();
     const router = useRouter();
-
+    const currentPath = usePathname();
     return (
         <div className={"text-left bg-background accent-red-50 p-2 mb-3"}>{
             session?
                 <div className={'space-x-3 flex'}>
                     <span className={'mr-5'}>{session.user.name}</span>
                     <Button onClick={() => signOut()}>Sign Out</Button>
-                    <Button onClick={() => router.push('/')}>Play with friends</Button>
-                    <Button variant={'blue'}
+                    
+                    {currentPath.match('/shuffle') === null ? <Button
                                    onClick={() => router.push('/shuffle')}>
                         Play with strangers
-                    </Button>
+                    </Button> : <Button onClick={() => router.push('/')}>Play with friends</Button>}
                 </div>
                 :
                 <Button onClick={() => signIn('discord', { callbackUrl: '/' })}>Sign In</Button>
